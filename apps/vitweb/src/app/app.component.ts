8b770000fd64df2742ff9489620b698bf0c5e385
nnuriozturk <nnuriozturk@hotmail.com>
2021-01-16 01:07:40 +0300

    nx 11 upgrade

---
+++ apps/vitweb/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vitweb-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'vitweb';
+}
